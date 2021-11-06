472d1c1df1d97dd0aaf7f2283a6bc1ac690ec960
PremanandChowdhury <freudigprem@gmail.com>
2021-11-06 22:57:31 +0530

    feat: Topic Simple Component of Storybook completed with PropTypes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div className='' list-item>
-      <input type='text' value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className='checkbox'>
+        <input
+          type='checkbox'
+          defaultChecked={state === 'TASK_ARCHIEVED'}
+          disabled={true}
+          name='checked'
+        />
+        <span className='checkbox-custom' onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className='title'>
+        <input
+          type='text'
+          value={title}
+          readOnly={true}
+          placeholder='Input title'
+        />
+      </div>
+      <div className='actions' onClick={(e) => e.stopPropagation()}>
+        {state !== 'TASK_ARCHIEVED' && (
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   )
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
 }
 
 export default Task
