9010530f7c8ea4da0b9811abffc7f7ea5ea77973
PremanandChowdhury <freudigprem@gmail.com>
2021-11-29 23:26:23 +0530

    feat: Nested container components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg'
-import './App.css'
+import { Provider } from 'react-redux'
+import store from './lib/redux'
 
-function App() {
+import InboxScreen from './components/InboxScreen'
+
+import './index.css'
+
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   )
 }
 
