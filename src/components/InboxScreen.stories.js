143b8964b9e53d982bef2393ec6d4324c5a0ec50
PremanandChowdhury <freudigprem@gmail.com>
2021-11-29 23:52:37 +0530

    feat: Supplying context with decorators

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { PureInboxScreen } from './InboxScreen'
+
+import { action } from '@storybook/addon-actions'
+import * as TaskListStories from './TaskList.stories'
+
+// A super-simple mock of a redux store
+const store = {
+  getState: () => {
+    return {
+      tasks: TaskListStories.Default.args.tasks,
+    }
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch'),
+}
 
 export default {
   component: PureInboxScreen,
+  decorators: [(story) => <Provider store={store}> {story()}</Provider>],
   title: 'InboxScreen',
 }
 
