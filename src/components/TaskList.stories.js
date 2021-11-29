5697f275c6e84e4f754130afae3ad0b88eb91527
PremanandChowdhury <freudigprem@gmail.com>
2021-11-29 22:43:31 +0530

    update: Story and Test file

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import TaskList from './TaskList'
+import { PureTaskList } from './TaskList'
 import * as TaskStories from './Task.stories'
 
 export default {
-  component: TaskList,
+  component: PureTaskList,
   title: 'TaskList',
   decorators: [
     (story) => (
@@ -17,7 +17,7 @@ export default {
   ],
 }
 
-const Template = (args) => <TaskList {...args} />
+const Template = (args) => <PureTaskList {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
