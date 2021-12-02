5fe06eda4af10f3319488937d585f41c5b17110f
PremanandChowdhury <freudigprem@gmail.com>
2021-12-02 23:48:17 +0530

    test: Changes for Visual Regrssion Test

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -39,7 +39,7 @@ export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
 
   if (tasks.length === 0) {
     return (
-      <div className='list-items'>
+      <div className='list-items' style={{ margin: '10px' }}>
         <div className='.wrapper-message'>
           <span className='icon-check' />
           <div className='title-message'>You have no tasks</div>
